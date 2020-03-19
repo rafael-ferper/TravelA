9372822da05a804d9c3c185f58dacb302bdd4c5b
rafael-ferper <rtio.fernandesp@gmail.com>
2020-03-19 14:00:24 -0300

    new js file

---
+++ js/myjs.js
@@ -0,0 +1,104 @@
+var searchOrigin = 'origin';
+		var searchDest = 'destination';
+
+		$(document).ready(function(){
+			var autocomplete;
+			autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchOrigin)));
+		});
+
+		$(document).ready(function(){
+			var autocomplete;
+			autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchDest)));
+			console.log(tform);
+		});
+
+		$(document).ready(function(){
+			var SPMaskBehavior = function (val) {
+				return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+			},
+			spOptions = {
+				onKeyPress: function(val, e, field, options) {
+					field.mask(SPMaskBehavior.apply({}, arguments), options);
+				}
+			};
+
+			$('#phone').mask(SPMaskBehavior, spOptions);
+		});
+
+		$(document).ready(function(){
+			$("#tform").validate({
+				rules:{
+					name:{
+						required: true,
+						maxlength: 50
+					},
+					phone:{
+						required: true
+					},	
+					origin:{
+						required: true,
+						maxlength:100
+					},
+					destination:{
+						required: true,
+						maxlength:100
+					},
+					dateTo:{
+						required: true,
+						date: true
+					},
+					dateFrom:{
+						required: true,
+						date: true
+					},
+					tnumber:{
+						required: true,
+						min: 1
+					}
+				}	
+			});
+		});
+
+		
+
+		
+
+		window.onload = function(){
+
+			var form = document.getElementById("tform");
+			var onsubmit = function(event){
+				event.preventDefault();
+
+				if(!locat() || !checkdate()){
+				
+				}else{
+					form.submit();
+				}
+			}
+
+			form.addEventListener("submit", onsubmit);
+		}
+
+		function locat() {
+			var orig = document.getElementById('origin').value;
+			var dest = document.getElementById('destination').value;
+
+			if (orig == dest) {
+				alert('O destino deve ser diferente da origem!');
+				return false;
+			}
+			return true;
+		}
+
+		function checkdate(){
+			var tform = document.tform;
+			var date_1 = new Date(tform.dateTo.value);
+			var date_2 = new Date(tform.dateFrom.value);
+			if (!date_1 || !date_2) return false;
+			if (date_1 > date_2) {
+				alert("Data from não pode ser menor que date to!!");	
+				return false;
+			} else {
+				return true;
+			}
+		}
